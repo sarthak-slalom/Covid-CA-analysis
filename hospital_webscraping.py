3829a146de570204e597410c161239329dc1fe7d
Maryam Veisi <mveisi@apple.com>
2020-03-17 18:56:32 -0700

    hospital web scraping- saving output into a csv file

--- hospital_webscraping.py
+++ hospital_webscraping.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import csv
 
 hospital_wait_time ={}
 url = 'https://goodsamsanjose.com/about/er-wait-times.dot'
@@ -8,3 +9,7 @@ table =  driver.find_element_by_xpath("//table[@class='table table-striped']")
 for row in table.find_elements_by_xpath(".//tr"):
     hospital_wait_time[[td.text for td in row.find_elements_by_xpath(".//td")][0]] = 
 [td.text for td in row.find_elements_by_xpath(".//td")][1]
+
+with open('test.csv', 'w') as f:
+    for key in hospital_wait_time.keys():
+        f.write("%s,%s\n"%(key,hospital_wait_time[key]))
